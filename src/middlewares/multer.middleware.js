e6638c4e2bcd7a5165787eba2dac825c13b46000
Shahid Hameed <shahidhameed32121@gmail.com>
2026-01-01 21:11:14 -0800

    fix multer temp path

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,18 +1,17 @@
 const multer = require("multer");
 
-// Hum file ko disk (hard drive) par save karenge temporary taur par
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public/temp") // File yahan save hogi
+        // Vercel par sirf /tmp folder allowed hota hai
+        cb(null, "/tmp")
     },
     filename: function (req, file, cb) {
-      // File ka original naam hi rakho
-      cb(null, file.originalname)
+        cb(null, file.originalname)
     }
-  })
-  
-const upload = multer({ 
-    storage: storage, 
-})
+});
 
-module.exports = { upload }
+const upload = multer({ 
+    storage, 
+});
+
+module.exports = { upload };
